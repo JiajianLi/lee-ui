2bb2b77922f9d13014c0f123bb6934d6ba62b6f7
jiajian <jiajianli@futunn.com>
2021-09-08 16:21:02 +0800

    fix: run lint

--- vue3-json-schema-form/src/components/MonacoEditor.tsx
+++ vue3-json-schema-form/src/components/MonacoEditor.tsx
@@ -1,112 +1,127 @@
-import { defineComponent, ref, onMounted, watch, onBeforeUnmount, shallowReadonly, shallowReactive, shallowRef } from "vue";
+import {
+  defineComponent,
+  ref,
+  onMounted,
+  watch,
+  onBeforeUnmount,
+  shallowReadonly,
+  shallowReactive,
+  shallowRef,
+} from "vue"
 
-import * as Monaco from 'monaco-editor'
+import * as Monaco from "monaco-editor"
 
-import type { PropType, Ref } from 'vue'
-import { createUseStyles } from 'vue-jss'
+import type { PropType, Ref } from "vue"
+import { createUseStyles } from "vue-jss"
 
 const useStyles = createUseStyles({
-    container: {
-        border: '1px solid #eee',
-        display: 'flex',
-        flexDirection: 'column',
-        borderRadius: 5
-    },
-    title: {
-        backgroundColor: '#eee',
-        padding: '10px 0',
-        paddingLeft: 20,
-    },
-    code: {
-        flexGrow: 1
-    }
+  container: {
+    border: "1px solid #eee",
+    display: "flex",
+    flexDirection: "column",
+    borderRadius: 5,
+  },
+  title: {
+    backgroundColor: "#eee",
+    padding: "10px 0",
+    paddingLeft: 20,
+  },
+  code: {
+    flexGrow: 1,
+  },
 })
 
 export default defineComponent({
-    props: {
-        code: {
-            type: String as PropType<string>,
-            required: true
-        },
-        onChange: {
-            // type: Function as PropType<(value: string, event: Monaco.editor.IModelChangedEvent) => {}>,
-            // type: Function as PropType<(value: string, event: any) => {}>,
-            type: Function,
-            required: true
-        },
-        title: {
-            type: String as PropType<string>,
-            required: true
-        }
+  props: {
+    code: {
+      type: String as PropType<string>,
+      required: true,
     },
-    setup(props) {
-        // must be shallowRef, if not, editor.getValue() won't work
-        const editorRef = shallowRef()
+    onChange: {
+      // type: Function as PropType<(value: string, event: Monaco.editor.IModelChangedEvent) => {}>,
+      // type: Function as PropType<(value: string, event: any) => {}>,
+      type: Function,
+      required: true,
+    },
+    title: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+  setup(props) {
+    // must be shallowRef, if not, editor.getValue() won't work
+    const editorRef = shallowRef()
 
-        const containerRef = ref()
+    const containerRef = ref()
 
-        let _subscription: Monaco.IDisposable | undefined
-        let __prevent_trigger_change_event = false
-        onMounted(() => {
-            const editor = editorRef.value = Monaco.editor.create(containerRef.value, {
-                value: props.code,
-                language: 'json',
-                formatOnPaste: true,
-                tabSize: 2,
-                minimap: {
-                    enabled: false,
-                },
-            })
-
-            _subscription = editor.onDidChangeModelContent(event => {
-                console.log('------------->', __prevent_trigger_change_event)
-                if (!__prevent_trigger_change_event) {
-                    props.onChange(editor.getValue(), event);
-                }
-            });
-        })
-
-        onBeforeUnmount(() => {
-            if (_subscription)
-                _subscription.dispose()
-        })
-
-        watch(() => props.code, v => {
-            const editor = editorRef.value
-            const model = editor.getModel()
-            if (v !== model.getValue()) {
-                editor.pushUndoStop();
-                __prevent_trigger_change_event = true
-                // pushEditOperations says it expects cursorComputer, but doesn't seem to need one,.....
-                model.pushEditOperations(
-                    [],
-                    [
-                        {
-                            range: model.getFullModelRange(),
-                            text: v,
-                        }
-                    ]
-                );
-                editor.pushUndoStop();
-                __prevent_trigger_change_event = false
-            }
-            // if (v !== editorRef.value.getValue()) {
-            //     editorRef.value.setValue(v)
-            // }
-        })
-
-        const classesRef = useStyles()
-
-        return () => {
-
-            const classes = classesRef.value
-
-            return (
-                <div class={classes.container}>
-                    <div class={classes.title}><span>{props.title}</span></div>
-                    <div class={classes.code} ref={containerRef}></div>
-                </div>
-            )
+    let _subscription: Monaco.IDisposable | undefined
+    let __prevent_trigger_change_event = false
+    onMounted(() => {
+      const editor = (editorRef.value = Monaco.editor.create(
+        containerRef.value,
+        {
+          value: props.code,
+          language: "json",
+          formatOnPaste: true,
+          tabSize: 2,
+          minimap: {
+            enabled: false,
+          },
         }
+      ))
+
+      _subscription = editor.onDidChangeModelContent(event => {
+        console.log("------------->", __prevent_trigger_change_event)
+        if (!__prevent_trigger_change_event) {
+          props.onChange(editor.getValue(), event)
+        }
+      })
+    })
+
+    onBeforeUnmount(() => {
+      if (_subscription) _subscription.dispose()
+    })
+
+    watch(
+      () => props.code,
+      v => {
+        const editor = editorRef.value
+        const model = editor.getModel()
+        if (v !== model.getValue()) {
+          editor.pushUndoStop()
+          __prevent_trigger_change_event = true
+          // pushEditOperations says it expects cursorComputer, but doesn't seem to need one,.....
+          model.pushEditOperations(
+            [],
+            [
+              {
+                range: model.getFullModelRange(),
+                text: v,
+              },
+            ]
+          )
+          editor.pushUndoStop()
+          __prevent_trigger_change_event = false
+        }
+        // if (v !== editorRef.value.getValue()) {
+        //     editorRef.value.setValue(v)
+        // }
+      }
+    )
+
+    const classesRef = useStyles()
+
+    return () => {
+      const classes = classesRef.value
+
+      return (
+        <div class={classes.container}>
+          <div class={classes.title}>
+            <span>{props.title}</span>
+          </div>
+          <div class={classes.code} ref={containerRef}></div>
+        </div>
+      )
     }
+  },
 })
--- vue3-json-schema-form/src/demos/simple.ts
+++ vue3-json-schema-form/src/demos/simple.ts
@@ -1,44 +1,44 @@
 export default {
-    name: 'Simple',
-    schema: {
-        description: 'A simple form example',
-        type: 'object',
-        required: ['firstName', 'lastName'],
-        properties: {
-            firstName: {
-                type: 'string',
-                default: 'Chuck'
-            },
-            lastName: {
-                type: 'string'
-            },
-            telephone: {
-                type: 'string',
-                minLength: 10
-            }
-        }
+  name: "Simple",
+  schema: {
+    description: "A simple form example",
+    type: "object",
+    required: ["firstName", "lastName"],
+    properties: {
+      firstName: {
+        type: "string",
+        default: "Chuck",
+      },
+      lastName: {
+        type: "string",
+      },
+      telephone: {
+        type: "string",
+        minLength: 10,
+      },
     },
-    uiSchema: {
-        title: 'A registration form',
-        properties: {
-            firstName: {
-                type: 'string',
-                default: 'Chuck'
-            },
-            lastName: {
-                type: 'string'
-            },
-            telephone: {
-                type: 'string',
-                minLength: 10
-            }
-        }
+  },
+  uiSchema: {
+    title: "A registration form",
+    properties: {
+      firstName: {
+        type: "string",
+        default: "Chuck",
+      },
+      lastName: {
+        type: "string",
+      },
+      telephone: {
+        type: "string",
+        minLength: 10,
+      },
     },
-    default: {
-        firstName: 'Chuck',
-        lastName: 'Norris',
-        age: 75,
-        bio: 'Roundhose kicking asses since 1940',
-        password: 'nomeed'
-    }
+  },
+  default: {
+    firstName: "Chuck",
+    lastName: "Norris",
+    age: 75,
+    bio: "Roundhose kicking asses since 1940",
+    password: "nomeed",
+  },
 }
